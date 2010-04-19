b493c69aff5050d3d5b437e73a2b668a314f187a
Thierry Martins <tmartins@nuxeo.com>
2010-04-19 19:35:56 +0200

    NXP-5031: Add a monitor to know synchonization status and cancel job

---
+++ nuxeo-platform-sync-client-api/src/main/java/org/nuxeo/ecm/platform/sync/api/util/AbstractSynchronizeMonitor.java
@@ -0,0 +1,33 @@
+package org.nuxeo.ecm.platform.sync.api.util;
+
+public class AbstractSynchronizeMonitor implements SynchronizeMonitor {
+
+    public void beginTask(String name, int totalWork) {
+
+    }
+
+    public void done() {
+
+    }
+
+    public boolean isCanceled() {
+        return false;
+    }
+
+    public void setCanceled(boolean value) {
+
+    }
+
+    public void setTaskName(String value) {
+
+    }
+
+    public void subTask(String value) {
+
+    }
+
+    public void worked(int work) {
+
+    }
+
+}
---
+++ nuxeo-platform-sync-client-api/src/main/java/org/nuxeo/ecm/platform/sync/api/util/SynchronizeMonitor.java
@@ -0,0 +1,20 @@
+package org.nuxeo.ecm.platform.sync.api.util;
+
+public interface SynchronizeMonitor {
+	
+    public  boolean isCanceled() ;
+
+    public  void setCanceled(boolean value) ;
+    
+    public void beginTask(String name, int totalWork);
+    
+    public  void setTaskName(String value) ;
+    
+    public  void subTask(String value) ;
+    
+    public  void worked(int work) ;
+    
+    public  void done() ;
+    
+    
+}
