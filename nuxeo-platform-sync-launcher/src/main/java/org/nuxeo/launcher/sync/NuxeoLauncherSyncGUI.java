5a9c2e694bb1dc8f1e72b1b81d056b1c16f27b2b
Stephane Lacoin <slacoin@nuxeo.com>
2012-09-19 18:03:19 +0200

    NXP-10152 added sync client automation and launcher module

---
+++ nuxeo-platform-sync-launcher/src/main/java/org/nuxeo/launcher/sync/NuxeoLauncherSyncGUI.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Sun Seng David TAN <stan@nuxeo.com>, jcarsique
+ */
+package org.nuxeo.launcher.sync;
+
+import org.nuxeo.launcher.NuxeoLauncher;
+import org.nuxeo.launcher.gui.NuxeoFrame;
+import org.nuxeo.launcher.gui.NuxeoLauncherGUI;
+
+/**
+ * Customized NuxeoLauncherGui to instantiate a {@link NuxeoSyncFrame} instead
+ * of {@link NuxeoFrame} {@link NuxeoSyncFrame} adds the sync tab.
+ *
+ * @author Sun Seng David TAN <stan@nuxeo.com>
+ *
+ */
+public class NuxeoLauncherSyncGUI extends NuxeoLauncherGUI {
+
+    public NuxeoLauncherSyncGUI(NuxeoLauncher launcher) {
+        super(launcher);
+    }
+
+    @Override
+    protected NuxeoFrame createNuxeoFrame(NuxeoLauncherGUI controller) {
+        return new NuxeoSyncFrame(controller);
+    }
+
+    @Override
+    public void updateServerStatus() {
+        super.updateServerStatus();
+        ((NuxeoSyncFrame) nuxeoFrame).updateSyncButton();
+    }
+
+}
